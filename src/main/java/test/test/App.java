0736efc5514ec810d975ad3ec0ad580574c27208
FanBJ <54fbj@163.com>
2017-04-12 11:29:16 +0800

    t

--- src/main/java/test/test/App.java
+++ src/main/java/test/test/App.java
@@ -1,11 +1,37 @@
 package test.test;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!Fanbj@!21`2");
+		String[] atp = { "Rafael Nadal", "Novak Djokovic", "Stanislas Wawrinka", "David Ferrer", "Roger Federer",
+				"Andy Murray", "Tomas Berdych", "Juan Martin Del Potro" };
+		List<String> players = Arrays.asList(atp);
+
+		// 以前的循环方式
+		for (String player : players) {
+			System.out.print(player + "; ");
+		}
+
+		// 使用 lambda 表达式以及函数操作(functional operation)
+		players.forEach((player) -> System.out.print(player + "; "));
+
+		// 在 Java 8 中使用双冒号操作符(double colon operator)
+		players.forEach(App::aa);
+		
+		
+	}
+
+	public static int a(int x, int y) {
+		return x + y;
+	}
+
+	public static void aa(String s) {
+		System.out.println("----" + s);
 	}
 }
